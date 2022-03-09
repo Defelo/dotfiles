d325c28d50d1aab6b74da4ee267752b354d47bbe
Defelo <elodef42@gmail.com>
2022-03-09 18:57:02 +0100

    Improved pwgen.sh

--- scripts/pwgen.sh
+++ scripts/pwgen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 length=$(rofi -dmenu -p Length)
+[[ $(echo -e 'Yes\nNo' | rofi -dmenu -p "Include symbols" -i -no-custom) = Yes ]] && symbols=y
 
-pwgen -sy $length 1 | tr -d '\n' | xclip -selection clipboard
+pwgen -s$symbols $length 1 | tr -d '\n' | xclip -selection clipboard
