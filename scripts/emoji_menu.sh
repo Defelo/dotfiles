ee5ff5cd39937b2412e7f953dc8efd98bb885d26
Defelo <elodef42@gmail.com>
2022-03-12 01:36:19 +0100

    Fixed emoji_menu.sh

--- scripts/emoji_menu.sh
+++ scripts/emoji_menu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-emoji=$(cat ~/.i3scripts/emoji_map.json | jq -r 'to_entries[] | .value + " :" + .key + ":"' | rofi -dmenu | cut -d' ' -f1)
+emoji=$(cat ~/scripts/emoji_map.json | jq -r 'to_entries[] | .value + " :" + .key + ":"' | rofi -dmenu | cut -d' ' -f1)
 printf "$emoji" | xclip -selection clipboard
 sleep .01
 if [ $(xdotool getwindowname $(xdotool getactivewindow)) = "st" ]; then
