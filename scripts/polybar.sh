508b017dd33d0a0ea7810ed6131416ff1d66ca85
Defelo <elodef42@gmail.com>
2022-05-23 21:55:52 +0200

    [scripts] Fixed polybar.sh

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    if [[ $m = "eDP1" ]]; then
+primary=$(polybar --list-monitors | grep '(primary)' | cut -d: -f1)
+
+for m in $(polybar --list-monitors | cut -d: -f1); do
+    if [[ $m = "$primary" ]] || [[ -z "$primary" ]]; then
         tp=right
     else
         tp=none
